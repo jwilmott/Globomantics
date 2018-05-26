0b3bed199f56471d8771cb6ca8fcbd1fc5fd11a5
Ben Glasder <bglasder@gmail.com>
2018-05-26 07:50:01 -0500

    Add header component

---
+++ src/main-page/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Header = () => (
+    <header className="row">
+        <div className="col-md-5">
+            <img src={logo} className="logo" alt="logo"/>
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            Providing stuff worldwide!
+        </div>
+    </header>
+);
+
+export default Header;
