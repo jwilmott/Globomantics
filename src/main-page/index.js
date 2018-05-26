ad60de75270dbd46ed220a86b43523fdd33b9b1f
Ben Glasder <bglasder@gmail.com>
2018-05-26 08:21:01 -0500

    ignore idea

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -4,6 +4,25 @@ import './main-page.css';
 import Header from './header';
 
 class App extends Component {
+    fetchHouses = () => {
+        fetch('/houses.json')
+            .then(rsp => rsp.json())
+            .then(allHouses => {
+                this.allHouses = allHouses;
+                this.determineFeaturedHouse();
+            })
+    }
+
+    determineFeaturedHouse = () => {
+        if (this.allHouses) {
+            const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+            const featuredHouse = this.allHouses[randomIndex];
+            this.setState({ featuredHouse });
+        };
+    }
+
+
+
   render() {
     return (
         <div className="container">
